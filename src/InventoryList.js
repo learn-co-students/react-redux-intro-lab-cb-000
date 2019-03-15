d3df54d7f12fd86d16999a5d1e0d89eb519349fb
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-15 00:06:13 +0000

    Done.

---
+++ src/InventoryList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const InventoryList = (props) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+export default InventoryList ;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
+import {getInventoryItems} from './actions/inventoryItemsActions'
+import App from './App';
+
+const store = configureStore();
+store.dispatch(getInventoryItems())
 
 ReactDOM.render(
   <App />,
   document.getElementById('container')
 );
-
-
